a23e62131bc31d17a271b622decbd85b80816498
Lucas DEGRET <lucas.degret@outlook.fr>
2023-05-23 10:44:32 +0200

    Mise en place d'un header + Mise en place d'une nouvelle Route pour renvoyer une page decriptif par rapport à l'id de l'anime

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import Animes from "@pages/Animes";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import OneAnime from "@pages/OneAnime";
+import "./App.scss";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>My blog Anime</h1>
+        <header>
+          <h1>My blog Anime</h1>
+        </header>
         <Routes>
           <Route path="/" element={<Animes />} />
+          <Route path="/animes/:id" element={<OneAnime />} />
         </Routes>
       </div>
     </BrowserRouter>
